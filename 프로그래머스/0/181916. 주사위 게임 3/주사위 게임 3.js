ff0e1af651b9a38468ef5e4b44712dcdbb545ba8
Yerin Hong <164183193+hhongye@users.noreply.github.com>
2025-06-10 18:00:25 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,28 +1,4 @@
-function solution(a1, b1, c1, d1) {
-  var answer = 0;
-
-  const dice = [a1, b1, c1, d1].sort((x, y) => x - y); 
-  const [a, b, c, d] = dice;
-
-  if(a === d){
-    answer = 1111 * a;
-  }else if(a === c || b === d){ 
-    const p = b;
-    const q = a === c ? d : a;
-    answer = (10 * p + q) ** 2;
-  }else if(a === b && c === d){ 
-    answer = (a + c) * Math.abs(a - c);
-  }else if(a === b || b === c || c === d){ 
-    if(a === b){
-      answer = c * d;
-    }else if(b === c){
-      answer = a * d;
-    }else{
-      answer = a * b;
-    }
-  }else{
-    answer = a;
-  }
-
-  return answer;
+function solution(a, b, c, d) {
+    var answer = 0;
+    return answer;
 }
