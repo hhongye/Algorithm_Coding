bb6a306eb39bcd9b2905ec55a8cca9b9b70315c3
Yerin Hong <164183193+hhongye@users.noreply.github.com>
2025-06-16 17:18:40 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, is_prefix) {
+  var answer = 1;
+  for(let i=0; i<is_prefix.length; i++){
+    if(my_string[i] !== is_prefix[i]){
+      answer = 0;
+      break;
+    }
+  }
+  return answer;
+}
